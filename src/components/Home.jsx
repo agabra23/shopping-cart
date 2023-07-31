68bc56f3f7b1990e0aa699fc2d9185a28f0d146f
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-07-31 10:14:10 -0400

    Style Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,17 @@
+import "../styles/Home.css";
+import { NavLink } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="container">
+      <div className="header-text">
+        <h1>Welcome to Finley's Cheese!</h1>
+        <h3>Shop for Finley's Cheese Today.</h3>
+
+        <NavLink to="shop" className="home-shop-button">
+          <button>Shop</button>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
